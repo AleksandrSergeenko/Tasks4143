3ec71b0087b3df87e3d5ecbf57370de2540bc432
AleksandrSergeenko <122687809+AleksandrSergeenko@users.noreply.github.com>
2023-03-04 21:05:28 +0300

    Add решение task 41

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@
 1, -7, 567, 89, 223-> 3
  */
 
-/* 
 Console.WriteLine($"Введите числа: ");
 int[] array = Console.ReadLine()!.Split(' ', ',', '.').Where(i => int.TryParse(i, out _)).Select(int.Parse).ToArray();
 int sum = 0;
@@ -14,30 +13,3 @@ foreach (int i in array)
         sum++;
 
 Console.WriteLine($"Чисел больше ноля введено {sum}");
- */
-
-/* 
-Задача 43: Напишите программу, которая найдёт точку пересечения 
-двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
-значения b1, k1, b2 и k2 задаются пользователем.
-b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
- */
-/* 
-Console.Write("k1 = ");
-double k1 = Convert.ToDouble(Console.ReadLine()!);
-Console.Write("b1 = ");
-double b1 = Convert.ToDouble(Console.ReadLine()!);
-Console.Write("k2 = ");
-double k2 = Convert.ToDouble(Console.ReadLine()!);
-Console.Write("b2 = ");
-double b2 = Convert.ToDouble(Console.ReadLine()!);
-
-if(k1 != k2)
-{
-    double x = (b1 - b2) / (k2 - k1);
-    double y = k1 * x + b1;
-    Console.WriteLine($"Пересечение в точке ({x}; {y})");
-}
-else 
-    Console.WriteLine($"Решения нет");
- */
